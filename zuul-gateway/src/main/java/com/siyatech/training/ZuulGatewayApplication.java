d5016e71950b4c71c83562c0de5cb149704b6b46
Raj gs <rajmatti@gmail.com>
2022-03-05 11:30:05 +0530

    zuul comit

---
+++ zuul-gateway/src/main/java/com/siyatech/training/ZuulGatewayApplication.java
@@ -0,0 +1,22 @@
+package com.siyatech.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+	
+	 @Bean
+	  public SimpleFilter simpleFilter() {
+	     return new SimpleFilter();
+	  }
+}
---
+++ zuul-gateway/src/test/java/com/siyatech/training/ZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siyatech.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
